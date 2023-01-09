9878a87a597518e879ace010bebe955e31e1eb76
Herri Purba <119906399+HerriUkirama@users.noreply.github.com>
2023-01-09 16:56:41 +0700

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
+  devise_for :models
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :posts, only: [:new, :create, :index]
 end
